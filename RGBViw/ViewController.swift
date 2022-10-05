b6079d1c62dcf42a67bacaf10bfc7a832245afbb
Vlad <gvladislav96@gmail.com>
2022-10-06 00:07:13 +0500

    Setting ViewController

--- RGBViw/ViewController.swift
+++ RGBViw/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rgbView: UIView!
+    
+    
+    @IBOutlet weak var redNumberLabel: UILabel!
+    @IBOutlet weak var greenNumberLabel: UILabel!
+    @IBOutlet weak var blueNumberLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rgbView.layer.cornerRadius = 20
+        
+        
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        redNumberLabel.text = String(redSlider.value)
+        greenNumberLabel.text = String(greenSlider.value)
+        blueNumberLabel.text = String(blueSlider.value)
+        }
+   
+    @IBAction func redSliderAction() {
+        redNumberLabel.text = String(redSlider.value)
+        
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        }
+    
+    @IBAction func greenSliderValue() {
+        greenNumberLabel.text = String(greenSlider.value)
+        
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-
-
+    
+    @IBAction func blueSliderAction() {
+        blueNumberLabel.text = String(blueSlider.value)
+        
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
 }
 
